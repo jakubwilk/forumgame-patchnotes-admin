714fe37d79d05abb4e0e690d0421373b37d20e29
jakubwilk <jw@jakubwilk.pl>
2024-01-07 16:46:44 +0100

    Install mantine UI

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,5 +2,15 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '36rem',
+        'mantine-breakpoint-sm': '48rem',
+        'mantine-breakpoint-md': '62rem',
+        'mantine-breakpoint-lg': '75rem',
+        'mantine-breakpoint-xl': '88rem',
+      },
+    },
   },
 }
--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
+import { createTheme, MantineProvider } from '@mantine/core'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { AppWrapper } from './AppWrapper'
 
+import '@mantine/core/styles.css'
+
+const theme = createTheme({})
+
+export default function App() {
   return (
-    <div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
+    <MantineProvider theme={theme}>
+      <AppWrapper />
+    </MantineProvider>
   )
 }
-
-export default App
