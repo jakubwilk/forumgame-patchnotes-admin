67ef1047910fb5cbbae0c2b2bc07ff800a45dc78
jakubwilk <jw@jakubwilk.pl>
2024-01-07 02:59:31 +0100

    Fix issue with running typescript in dev server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,25 @@
-import fastify from 'fastify'
+import Fastify from 'fastify'
 
-const startServer = () => {
-  const server = fastify({
-    logger: !!(process.env.NODE_ENV !== 'development'),
+const fastify = Fastify({
+  logger: !!(process.env.NODE_ENV !== 'development'),
+})
+const PORT = Number(process.env.PORT) || 3006
+
+const initializeRoutes = () => {
+  fastify.get('/', async () => {
+    return { hello: 'world' }
   })
-  const PORT = Number(process.env.PORT) || 3006
-
-  server.listen({ port: PORT })
-  console.log(`🚀  Fastify server running on port http://localhost:${PORT}`)
 }
 
-startServer()
+const start = async () => {
+  try {
+    initializeRoutes()
+    await fastify.listen({ port: PORT })
+    console.log(`🚀  Fastify server running on port http://localhost:${PORT}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
