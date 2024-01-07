cc9a15a3c178ec7f469ab01d2196b697f775fe25
jakubwilk <jw@jakubwilk.pl>
2024-01-07 02:39:58 +0100

    Fix scope for main package.json and add ts-node and update package script in server dir

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
 
-const server = fastify({
-  logger: !!(process.env.NODE_ENV !== 'development'),
-})
-const PORT = Number(process.env.PORT) || 3006
+const startServer = () => {
+  const server = fastify({
+    logger: !!(process.env.NODE_ENV !== 'development'),
+  })
+  const PORT = Number(process.env.PORT) || 3006
 
-server.listen({ port: PORT })
+  server.listen({ port: PORT })
+  console.log(`🚀  Fastify server running on port http://localhost:${PORT}`)
+}
 
-console.log(`🚀  Fastify server running on port http://localhost:${PORT}`)
+startServer()
