2f7c6b0777599eddd5db8331a37aee47e85753d0
jakubwilk <jw@jakubwilk.pl>
2024-01-07 16:50:41 +0100

    Install react-router

--- client/src/app/AppWrapper.tsx
+++ client/src/app/AppWrapper.tsx
@@ -1,24 +1,7 @@
-import { useState } from 'react'
+import { RouterProvider } from 'react-router-dom'
+
+import { ROUTES } from '../config/routes.config'
 
 export function AppWrapper() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      <h1>{'Vite + React'}</h1>
-      <div className={'card'}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          {'count is '}
-          {count}
-        </button>
-        <p>
-          {'Edit '}
-          <code>{'src/App.tsx'}</code> {'and save to test HMR\r'}
-        </p>
-      </div>
-      <p className={'read-the-docs'}>
-        {'Click on the Vite and React logos to learn more'}
-      </p>
-    </div>
-  )
+  return <RouterProvider router={ROUTES} />
 }
---
+++ client/src/config/routes.config.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+export const ROUTES = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>{'Hello world!'}</div>,
+  },
+])
