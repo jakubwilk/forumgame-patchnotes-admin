b8bbf24e160fbe647697ace45f7c6aff2ead85d2
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-02-24 00:15:27 +0100

    Fix theme settings and add tooltip with link to logo

--- client/src/app/config/theme.ts
+++ client/src/app/config/theme.ts
@@ -1,43 +1,11 @@
-import { MantineTheme } from '@mantine/core'
+import { createTheme } from '@mantine/core'
 
-export const THEME: Partial<MantineTheme> = {
+export const THEME = createTheme({
   primaryColor: 'orange',
   fontFamily: "'Lato', sans-serif",
   headings: {
     fontFamily: "'Rubik', sans-serif",
     fontWeight: '600',
     textWrap: 'wrap',
-    sizes: {
-      h1: {
-        fontSize: '3rem',
-        fontWeight: '600',
-        lineHeight: '3rem',
-      },
-      h2: {
-        fontSize: '2.5rem',
-        fontWeight: '600',
-        lineHeight: '2.5rem',
-      },
-      h3: {
-        fontSize: '2.25rem',
-        fontWeight: '600',
-        lineHeight: '2.25rem',
-      },
-      h4: {
-        fontSize: '2rem',
-        fontWeight: '600',
-        lineHeight: '2rem',
-      },
-      h5: {
-        fontSize: '1.5rem',
-        fontWeight: '600',
-        lineHeight: '1.5rem',
-      },
-      h6: {
-        fontSize: '1rem',
-        fontWeight: '600',
-        lineHeight: '1rem',
-      },
-    },
   },
-}
+})
--- client/src/modules/common/header/Logo.tsx
+++ client/src/modules/common/header/Logo.tsx
@@ -1,12 +1,17 @@
-import { Title } from '@mantine/core'
+import { Link } from 'react-router-dom'
+import { Title, Tooltip } from '@mantine/core'
 
 import classes from './Header.module.css'
 
 export function Logo() {
   return (
-    <Title order={1} className={classes.logoTitle}>
-      {'Patch'}
-      <span className={classes.logoText}>{'notes'}</span>
-    </Title>
+    <Tooltip label={'Powrót na stronę główną'} position={'bottom'} color={'gray'}>
+      <Link to={'/'}>
+        <Title order={1} className={classes.logoTitle}>
+          {'Patch'}
+          <span className={classes.logoText}>{'notes'}</span>
+        </Title>
+      </Link>
+    </Tooltip>
   )
 }
