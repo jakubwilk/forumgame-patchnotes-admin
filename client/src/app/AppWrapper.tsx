f68e5fbd8a9d44bdd8309836c5f4686b99d7ad6b
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-02-23 20:29:07 +0100

    Add tailwindcss and mantine ui

--- client/src/app/AppWrapper.tsx
+++ client/src/app/AppWrapper.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
 
 import { ROUTER } from './config'
 
+import '@mantine/core/styles.layer.css'
+
 export function AppWrapper() {
-  return <RouterProvider router={ROUTER} />
+  return (
+    <MantineProvider defaultColorScheme={'dark'} forceColorScheme={'dark'}>
+      <RouterProvider router={ROUTER} />
+    </MantineProvider>
+  )
 }
